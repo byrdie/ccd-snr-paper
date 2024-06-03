439d11a05242de413f1f87ac35695296a95e1314
Roy Smart <roytsmart@gmail.com>
2024-06-03 17:34:36 -0600

    updates

--- ccd_snr/_authors.py
+++ ccd_snr/_authors.py
@@ -21,13 +21,24 @@ def authors() -> list[aastex.Author]:
         "Goddard Space Flight Center, 8800 Greenbelt Rd, Greenbelt, MD 20771, USA"
     )
 
-    roy = aastex.Author("Roy T. Smart", affiliation=msu)
-    charles = aastex.Author("Charles C. Kankelborg", affiliation=msu)
-    jake = aastex.Author("Jacob D. Parker", affiliation=gsfc)
+    roy = aastex.Author(
+        name="Roy T. Smart",
+        affiliation=msu,
+        email="roytsmart@gmail.com"
+    )
+
+    charles = aastex.Author(
+        name="Charles C. Kankelborg",
+        affiliation=msu,
+    )
+
+    jake = aastex.Author(
+        name="Jacob D. Parker",
+        affiliation=gsfc,
+    )
 
     return [
         roy,
         charles,
         jake,
-        aastex.CorrespondingAuthor(roy.name, email="roytsmart@gmail.com"),
     ]
--- ccd_snr/_authors_test.py
+++
@@ -1,8 +0,0 @@
-import aastex
-import ccd_snr
-
-
-def test_authors():
-    result = ccd_snr.authors()
-    for author in result:
-        assert isinstance(author, (aastex.Author, aastex.CorrespondingAuthor))
